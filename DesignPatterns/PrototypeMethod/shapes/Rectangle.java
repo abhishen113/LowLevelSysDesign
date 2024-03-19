99fcb217344909ac710bbabad25b3c444ed4d8cc
abhishen113 <abhishen99@gmail.com>
2024-03-19 12:00:54 +0530

    ProtoType Method

---
+++ DesignPatterns/PrototypeMethod/shapes/Rectangle.java
@@ -0,0 +1,35 @@
+package DesignPatterns.PrototypeMethod.shapes;
+
+import java.util.Objects;
+
+public class Rectangle extends  Shape{
+
+
+    public int width;
+    public int height;
+
+
+    public Rectangle(){}
+
+    public Rectangle(Rectangle target){
+
+        super(target);
+        if(target!=null){
+            this.width = target.width;
+            this.height = target.height;
+        }
+
+    }
+
+    @Override
+    public Shape clone() {
+        return new Rectangle(this);
+    }
+
+    @Override
+    public boolean equals(Object object2) {
+        if(!(object2 instanceof  Shape)) return false;
+        Shape shape2 = (Shape) object2;
+        return shape2.x == x && shape2.y == y && Objects.equals(shape2.color , color);
+    }
+}
